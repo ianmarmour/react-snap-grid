a742af1649e99e817e24d09e66a3e286c8717491
Ian Armour <ianmarmour@gmail.com>
2020-03-11 12:29:37 -0700

    Added first functional app component and test.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+    <canvas
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onClick={e => {
+        alert(e.clientX)
+      }}
+    />
+  )
+}
+
+export default App
